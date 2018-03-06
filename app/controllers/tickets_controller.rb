41daadcb39ef32fdffd4afa123d4ff77c0da7ed6
Ahmed Elmi <ahmedelmi11@gmail.com>
2018-03-06 11:55:16 +0000

    WIP

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,2 +1,23 @@
 class TicketsController < ApplicationController
+  def create
+    @ticket = Ticket.new(tickets_params)
+    @ticket.student_id = current_user.id
+    if @ticket.save
+      redirect_to ticket_path(@ticket)
+    else
+      render :new
+    end
+  end
+
+  def new
+    @ticket = Ticket.new
+  end
+
+  private
+    def tickets_params
+      params.require(:ticket).permit(:description, :finished, :title)
+    end
+
 end
+
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180305182521) do
+ActiveRecord::Schema.define(version: 20180306112648) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,8 +46,9 @@ ActiveRecord::Schema.define(version: 20180305182521) do
     t.boolean "finished", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "student_id"
     t.integer "teacher_id"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_tickets_on_user_id"
   end
 
   create_table "user_technologies", force: :cascade do |t|
@@ -78,6 +79,7 @@ ActiveRecord::Schema.define(version: 20180305182521) do
 
   add_foreign_key "ticket_technologies", "technologies"
   add_foreign_key "ticket_technologies", "tickets"
+  add_foreign_key "tickets", "users"
   add_foreign_key "user_technologies", "technologies"
   add_foreign_key "user_technologies", "users"
 end
