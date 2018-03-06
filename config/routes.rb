e0e71bc98ef8d0eeaf7fade530f9cc708839acad
David O'Neill daveyoneill123@gmail.com <daveyoneill123@gmail.com>
2018-03-06 12:56:16 +0000

    sorted edit form

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :ticket, only: [:edit, :update]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
  resources :tickets, only: [:new, :create, :edit, :show, :index]
 end
