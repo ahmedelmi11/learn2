987af77c99e41cef5c81a26ba8a8a1a6637b0768
David O'Neill daveyoneill123@gmail.com <daveyoneill123@gmail.com>
2018-03-06 12:16:44 +0000

    edit controller

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,2 +1,19 @@
 class TicketsController < ApplicationController
+
+  def edit
+    @tickets = Ticket.find(params[:id])
+  end
+
+  def update
+    @ticket = Ticket.find(params[:id])
+    if @ticket.update(ticket_params)
+      flash[:notice] = "Ticket was succesfully updated"
+      redirect_to_ticket_path(@ticket)
+    else
+      render 'edit'
+    end
+  end
 end
+
+
+
