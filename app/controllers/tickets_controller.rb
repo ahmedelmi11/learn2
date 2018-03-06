f6de006d0591eceb8bb632f6629141ff5d53d912
David O'Neill daveyoneill123@gmail.com <daveyoneill123@gmail.com>
2018-03-06 12:30:42 +0000

    before action

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,17 +1,28 @@
 class TicketsController < ApplicationController
+  before_action :find_ticket, only: [:update, :edit]
 
   def edit
-    @tickets = Ticket.find(params[:id])
+
   end
 
   def update
-    @ticket = Ticket.find(params[:id])
+
     if @ticket.update(ticket_params)
       flash[:notice] = "Ticket was succesfully updated"
       redirect_to_ticket_path(@ticket)
     else
       render 'edit'
     end
+  end
+
+
+
+
+
+
+  private
+  def find_ticket
+    @ticket = Ticket.find(params[:id])
   end
 end
 
