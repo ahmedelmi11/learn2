bedccab658f6e7ed825b81ac0d8b804bddf3333f
David O'Neill daveyoneill123@gmail.com <daveyoneill123@gmail.com>
2018-03-07 14:18:09 +0000

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,12 +20,23 @@ python = Technology.create!(name: "Python")
 javascript = Technology.create!(name: "Javascript")
 java = Technology.create!(name: "Java")
 php = Technology.create!(name: "PHP")
+android = Technology.create!(name: "Android")
+ios = Technology.create!(name: "IOS")
 
 
 puts "Linking users to technologies"
 # linking user to technologies
 UserTechnology.create!(user: teacher, technology: html)
 UserTechnology.create!(user: teacher, technology: rails)
+UserTechnology.create!(user: teacher, technology: ruby)
+UserTechnology.create!(user: teacher, technology: python)
+UserTechnology.create!(user: teacher, technology: javascript)
+UserTechnology.create!(user: teacher, technology: java)
+UserTechnology.create!(user: teacher, technology: php)
+UserTechnology.create!(user: teacher, technology: android)
+UserTechnology.create!(user: teacher, technology: ios)
+
+
 
 # creating ticket
 ticket = Ticket.new(title: "My ruby ticket", description: "Some description")
