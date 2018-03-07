e626954a873d5e1e2d208abb4c01ade43545a2e7
David O'Neill daveyoneill123@gmail.com <daveyoneill123@gmail.com>
2018-03-07 11:21:15 +0000

    added list of tickets on show page of user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,9 @@ class User < ApplicationRecord
   has_many :user_technologies
   has_many :technologies, through: :user_technologies
   has_many :comments
-  has_many :tickets
+  # has_many :tickets
+  has_many :tickets_as_student, :class_name => "Ticket", :foreign_key => :student_id
+  has_many :tickets_as_teacher, :class_name => "Ticket", :foreign_key => :teacher_id
 
 
   def teacher?
