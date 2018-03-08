c2a4aeb0cde11bc83d609c5a08b090b0c23b3f69
Ahmed Elmi <ahmedelmi11@gmail.com>
2018-03-08 13:33:10 +0000

    got rid of extra line in schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20180307183339) do
-=======
-ActiveRecord::Schema.define(version: 20180307172120) do
->>>>>>> ed5b277f0a94c2a097b41fd61447b7066d2702fd
+
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
