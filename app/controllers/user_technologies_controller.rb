04f623a8a0be8eed78a71c67502cfaf1e55f80fc
David O'Neill daveyoneill123@gmail.com <daveyoneill123@gmail.com>
2018-03-09 12:49:35 +0000

    changed user technolgy form and amended technologies

--- app/controllers/user_technologies_controller.rb
+++ app/controllers/user_technologies_controller.rb
@@ -9,14 +9,21 @@ class UserTechnologiesController < ApplicationController
 
   def create
     @user = current_user
-    @technology = Technology.find(params[:user_technology][:technology_id])
-    @user_technology = UserTechnology.new(user: @user, technology: @technology)
-    @user_technology.save
+    create_user_technologies(@user)
     redirect_to user_path(@user)
   end
 
 
   def destroy
   end
+
+  def create_user_technologies(user)
+    UserTechnology.where(user: user).destroy_all
+    params[:user][:user_technologies][:technology_id].each do |tech_id|
+      user_technology = UserTechnology.new(technology_id: tech_id,  user_id: user.id)
+      user_technology.save
+    end
+  end
+
 
 end
