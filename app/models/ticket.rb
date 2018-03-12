1def0304f3ed390c475735895c03e84798fce749
Ahmed Elmi <ahmedelmi11@gmail.com>
2018-03-12 16:58:04 +0000

    nearly there

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,7 +3,7 @@ class Ticket < ApplicationRecord
   # has_one :teacher, foreign_key => "teacher_id", class_name => "User"
   belongs_to :student, :class_name => "User", :foreign_key => :student_id
   belongs_to :teacher, :class_name => "User", :foreign_key => :teacher_id, optional: true
-  has_many :ticket_technologies
+  has_many :ticket_technologies, dependent: :destroy
   has_many :technologies, through: :ticket_technologies
   monetize :price_cents
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180312162230) do
+ActiveRecord::Schema.define(version: 20180312163615) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20180312162230) do
     t.integer "status", default: 0
     t.integer "price_cents", default: 0, null: false
     t.string "state", default: "pending"
+    t.string "sku"
   end
 
   create_table "user_technologies", force: :cascade do |t|
